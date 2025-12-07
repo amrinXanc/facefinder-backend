4ce8d73a18f5a573acf98ebcbcd571ca38315b11
Nirmalya Chatterjee <2005juggernaut@gmail.com>
2025-12-07 06:30:34 +0530

    Update AuthController.js
    Removed the old reset password implementation and retained the new one.

--- Controllers/AuthController.js
+++ Controllers/AuthController.js
@@ -45,33 +45,6 @@ const forgotPassword = async (req, res) => {
     return res.status(500).json({ message: "Email service error", error: errorMessage });
   }
 };
-// ----------------- RESET PASSWORD -----------------
-const resetPassword = async (req, res) => {
-  try {
-    const { token } = req.params;
-    const { password } = req.body;
-
-    const user = await UserModel.findOne({
-      resetToken: token,
-      resetTokenExpiry: { $gt: Date.now() }
-    });
-
-    if (!user) {
-      return res.status(400).json({ message: "Invalid or expired token" });
-    }
-
-    const hashedPassword = await bcrypt.hash(password, 10);
-    user.password = hashedPassword;
-    user.resetToken = undefined;
-    user.resetTokenExpiry = undefined;
-    await user.save();
-
-    res.status(200).json({ message: "Password reset successful" });
-  } catch (err) {
-    console.error("Reset password error:", err);
-    res.status(500).json({ message: "Internal server error" });
-  }
-};
 
 // ----------------- RESET PASSWORD -----------------
 const resetPassword = async (req, res) => {
